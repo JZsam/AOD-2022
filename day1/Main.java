902360212b724f5ed3eb5f0373d0446e5b256c68
Jacob Samurin <jsamurin@Jacobs-Macbook.uaschools.org>
2022-12-05 13:45:38 -0500

    added .gitignore and finished day 1 part 2

--- day1/Main.java
+++ day1/Main.java
@@ -1,33 +1,28 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 class Main{
 	public static void main(String[] args) throws IOException{
+		// Part 1
+		// Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
 		Scanner f = new Scanner(new File("input"));
 		int numberOfElfs = 0;
-		// Map<Integer,ArrayList<Integer>> elfCalories = new HashMap<Integer,ArrayList<Integer>>();
 		Map<Integer,Integer> elfCalories = new TreeMap<Integer,Integer>();
 		while(f.hasNextLine()){
-			// ArrayList<Integer> items = new ArrayList<Integer>();
 			Boolean cont = false;
 			int sum=0;
 			while(f.hasNextLine()&&!cont){
 				String numString = f.nextLine();
-				// System.out.println(numString);
 				int num=0;
 				if(numString==""){
 					cont=true;
-					// System.out.println("New Elf");
 				}else{
 					num=Integer.parseInt(numString);
 					sum+=num;
-					// items.add(num);
-					// System.out.println(num);
 				}
 			}
 			elfCalories.put(numberOfElfs, sum);
@@ -42,19 +37,22 @@ class Main{
 			if(elfTotalCalories.get(bigest)<elfTotalCalories.get(j))
 				bigest=j;
 		}
-
-		// while(i.hasNext()){
-		// 	ArrayList<Integer> arr=i.next();
-		// 	int total = 0;
-		// 	for(Integer item:arr)
-		// 		total+=item;
-		// 	elfTotalCalories.add(total);
-		// }
-		// int large=0;
-		// for(int j=1;j>elfTotalCalories.size();j++){
-		// 	if(elfTotalCalories.get(large)<elfTotalCalories.get(j))
-		// 		large=j;
-		// }
-		System.out.println("Elf :: " + bigest +"\nCalories :: " +elfTotalCalories.get(bigest));
+		// Print
+		System.out.println("Part 1"+"\nElf :: " + bigest +"\nCalories :: " +elfTotalCalories.get(bigest));
+		// Part 2
+		// Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+		int top=0,top1=0,top2=0;
+		for(int j=0;j<elfTotalCalories.size();j++){
+			if(elfTotalCalories.get(top2)<elfTotalCalories.get(j))
+				if(elfTotalCalories.get(top1)<elfTotalCalories.get(j))
+					if(elfTotalCalories.get(top)<elfTotalCalories.get(j))
+						top=j;
+					else
+						top1=j;
+				else
+					top2=j;
+		}
+		// Print
+		System.out.println("Part 2"+"\nTop :: " + top + " :: Calories :: " + elfTotalCalories.get(top) + "\nTop1 :: " + top1 + " :: Calories :: " + elfTotalCalories.get(top1) + "\nTop2 :: " +top2+ " :: Calories :: " + elfTotalCalories.get(top2) + "\nTotal :: " +(elfTotalCalories.get(top)+elfTotalCalories.get(top1)+elfTotalCalories.get(top2)));
 	}
 }
