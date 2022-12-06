907071849f993f11a6673df33f9f3aee5e5936a9
Jzdoot <jacobsamurin@gmail.com>
2022-12-05 23:09:41 -0500

    set up Day 2

---
+++ day2/Main.java
@@ -0,0 +1,12 @@
+class Main{
+	private int abcScore=0, xyzScore=0;
+	void play (String abc, String xyz){
+		//a,x==rock
+		//b,y==paper
+		//c,z==scissors
+
+	}
+	public static void main(String[] args){
+
+	}
+}
